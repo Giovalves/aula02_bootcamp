87738bfaaafd731cf45580ae7c0ca4552ea8474b
Giovanna <giovalves2005@yahoo.com.br>
2024-06-22 18:32:31 -0300

    adicionado exercico 04

--- exercicios.py
+++ exercicios.py
@@ -1,11 +1,26 @@
 # Inteiro
 # 1) Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-      
+numero_1_1 = int(input("insira um numero inteiro: "))
+numero_1_2 = int(input("insira um numero inteiro: "))
+resultado_1 = numero_1_1 + numero_1_2
+print(resultado_1)
+
 # 2) Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero_2_1 = int(input("insira um numero inteiro: "))
+resultado_2 = numero_2_1 % 5
+print(f"O resto da divisão por 5 é: {resultado_2}")
+
 # 3) Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero_3_1 = int(input("Digito um numero: "))
+numero_3_2 = int(input("Digito um numero: "))
+resultado_3 = numero_3_1 * numero_3_2
+print(resultado_3)
+
 # 4) Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-numero_1 = int(input("insira um numero inteiro: "))
-numero_2 = int(input("insira um numero inteiro: "))
-resultado = numero_1 // numero_2
-print(resultado)
+numero_4_1 = int(input("insira um numero inteiro: "))
+numero_4_2 = int(input("insira um numero inteiro: "))
+resultado_4 = numero_4_1 // numero_4_2
+print(resultado_4)
 # 5) Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero_5_1 = int(input("Digite um numero: "))
+numero_5_2 = int(input("Digite um numero: "))
