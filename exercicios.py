9b47ec8d03129d4c09cf61557fdc790bc869d65c
Giovanna <giovalves2005@yahoo.com.br>
2024-06-22 18:04:37 -0300

    adicionado exercicio aula2

---
+++ exercicios.py
@@ -0,0 +1,11 @@
+# Inteiro
+# 1) Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+      
+# 2) Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# 3) Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 4) Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+numero_1 = int(input("insira um numero inteiro: "))
+numero_2 = int(input("insira um numero inteiro: "))
+resultado = numero_1 // numero_2
+print(resultado)
+# 5) Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
